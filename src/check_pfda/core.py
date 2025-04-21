29f32d47f3805ce6de66645b7f5b5655a53a9f7e
Ben Cressman <89537293+bencres@users.noreply.github.com>
2025-04-21 16:57:00 -0500

    Reformat config and refactor print to echo

--- src/check_pfda/core.py
+++ src/check_pfda/core.py
@@ -1,9 +1,11 @@
 """Collect tests and run them on supplied code."""
 
+from click import echo
+
 
 def check_student_code(assignment_id: str) -> None:
     """Check student code."""
-    print(f"Checking assignment {assignment_id}...")
+    echo(f"Checking assignment {assignment_id}...")
 
 
 """Collect results and format them."""
@@ -11,4 +13,4 @@ def check_student_code(assignment_id: str) -> None:
 
 def write_assignment_results(assignment_id: str) -> None:
     """Write assignment results to CSV."""
-    print(f"Writing results for assignment {assignment_id}...")
+    echo(f"Writing results for {assignment_id}...")

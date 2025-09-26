43849bbc3e09dc1eb98dc2a9f99f2a97d3a92b38
Ben Cressman <ben@utdallas.edu>
2025-09-26 09:02:11 -0500

    fix: remove unnecessary core logic now that the module is run as a script

--- src/check_pfda/core.py
+++ src/check_pfda/core.py
@@ -1,19 +1,16 @@
 """Collect tests and run them on supplied code."""
 
 import os
-
 from tempfile import NamedTemporaryFile
 import sys
 
 
+from click import echo
+import pytest
 from check_pfda.utils import get_current_assignment, get_tests
 
-from click import echo, secho
 
-import pytest
-
-
-def check_student_code(verbosity: int = 2, debug: bool = False) -> None:
+def check_student_code(verbosity: int = 2) -> None:
     """Check student code."""
     try:
         current = get_current_assignment()
@@ -26,18 +23,6 @@ def check_student_code(verbosity: int = 2, debug: bool = False) -> None:
     cwd_src = os.path.join(os.getcwd(), "src")
     if cwd_src not in sys.path:
         sys.path.insert(0, cwd_src)
-    if debug:
-        secho("\nIN DEBUG MODE\n", fg="blue", bold=True)
-        base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        test_path = os.path.join(base_dir, "check_pfda",
-                                 ".test_static_imports",
-                                 f"test_{assignment}.py")
-        args = [test_path]
-        if verbosity > 0:
-            args.append(f"-{'v' * verbosity}")
-        exit_code = pytest.main(args)
-        echo(f"Pytest finished with exit code {exit_code}")
-        return
     tests = get_tests(chapter, assignment)
 
     temp_file = NamedTemporaryFile(suffix=".py", delete=False)
@@ -49,7 +34,7 @@ def check_student_code(verbosity: int = 2, debug: bool = False) -> None:
         args = [temp_file.name]
         if verbosity > 0:
             args.append(f"-{'v' * verbosity}")
-        exit_code = pytest.main(args)
+        pytest.main(args)
     finally:
         os.remove(temp_file.name)
     if cwd_src in sys.path:

5fed9464cc76d9f8cb43b0f7887c682343876ca0
Ben Cressman <ben@utdallas.edu>
2025-09-26 08:54:47 -0500

    fix: remove unnecessary cli logic now that the module is ran as a script
    fix: remove unnecessary cli logic now that the module is run as a script

--- src/check_pfda/cli.py
+++ src/check_pfda/cli.py
@@ -9,21 +9,3 @@ from .core import check_student_code
 def cli():
     """Run student code checks."""
     check_student_code()
-
-
-# @cli.command()
-# @click.option(
-#     '-v',
-#     '--verbosity',
-#     count=True,
-#     help='Verbosity of test output'
-# )
-# @click.option(
-#     '-d',
-#     '--debug',
-#     is_flag=True,
-#     help='Swap to offline tests'
-# )
-# def check(verbosity: int, debug: bool) -> None:
-#     """Run student checks."""
-#     check_student_code(verbosity, debug)
